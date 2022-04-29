90d28cc1c4011eea7b0134c76360f5c351a7d015
Szczepan Wójcik <szczep1000@outlook.com>
2022-04-29 19:01:41 +0200

    Lab8_PR

---
+++ L8_PR/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import QuizComponent from "./components/quiz-component.jsx";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <QuizComponent/>
+  );
+}
+
+export default App
